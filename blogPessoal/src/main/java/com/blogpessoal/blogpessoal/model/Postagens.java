971b8fa848eaab02313d2702b3d6b09df5ccbd5a
kamilalimamf@gmail.com <marlon.falcao@gmail.com>
2022-05-04 11:48:13 -0300

    Atualizando CRUD - Blog pessoal

--- blogPessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagens.java
+++ blogPessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagens.java
@@ -6,11 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -35,6 +40,11 @@ public class Postagens {
 
 	
 	private String foto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+	
 
 
 	public long getId() {
@@ -88,9 +98,15 @@ public class Postagens {
 		this.foto = foto;
 	}
 	
-	
-	
-	
+	public Tema getTema() {
+		return tema;
+	}
 
 
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	
+	
 }
